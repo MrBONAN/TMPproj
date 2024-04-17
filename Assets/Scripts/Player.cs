bb1a563ee929789105cda97616020a70f33a84ae
MrBONAN <taleshatrycob@gmail.com>
2024-04-18 00:50:59 +0500

    Добавил анимашку + поврот спрайта + прыжки

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 500f;
+    public float jumpSpeed = 10f;
     private Rigidbody2D rb;
+    private SpriteRenderer texture;
+    public bool facingRight = true;
 
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        texture = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -18,8 +22,21 @@ public class Player : MonoBehaviour
 
     private void PlayerControl()
     {
-        var h = Input.GetAxis("Horizontal");
-        rb.velocity = transform.TransformDirection(new Vector3(h, 0, 0) * (speed * Time.fixedDeltaTime) +
-                                                   new Vector3(0, rb.velocity.y, 0));
+        var h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (h != 0 && h > 0 == facingRight)
+        {
+            facingRight = !facingRight;
+            texture.flipX = facingRight;
+        }
+        rb.velocity = transform.TransformDirection(new Vector2(h, rb.velocity.y));
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("platform") && Input.GetButton("Jump"))
+        {
+            rb.velocity = transform.TransformDirection(new Vector2(rb.velocity.x, jumpSpeed));
+        }
     }
 }
