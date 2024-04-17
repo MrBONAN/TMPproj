76c83842b3fc7074e97fb31dd448395ba45a6c1b
MrBONAN <taleshatrycob@gmail.com>
2024-04-17 21:48:05 +0500

    Добавлен игрок

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 100f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerControl();
+    }
+
+    private void PlayerControl()
+    {
+        var h = Input.GetAxis("Horizontal");
+        rb.velocity = transform.TransformDirection(new Vector3(h, 0, 0) * (speed * Time.fixedDeltaTime) +
+                                                   new Vector3(0, 0, 0));
+    }
+}
